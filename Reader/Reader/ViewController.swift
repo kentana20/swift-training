5aa0293a7e4e267e62f038239b7215074524a9d0
kentana20 <tanakak@eac.ne.jp>
2015-11-09 05:56:53 +0900

    [add] initial commit

---
+++ Reader/Reader/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Reader
+//
+//  Created by kensuke tanaka on 11/9/15.
+//  Copyright © 2015 kentana20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // どっかのサーバにHTTPリクエストを行い、データを取得
+        
+        // 取得したデータをViewに表示
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
